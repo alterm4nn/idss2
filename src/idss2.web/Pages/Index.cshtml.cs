42cb2d042bf42b6024b87d31b042e98d05856909
Evgenii Nurminskii <eugene@mstlab.org>
2022-03-30 22:07:30 +0200

    Initial commit - project scaffold created

---
+++ src/idss2.web/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace idss2.web.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
